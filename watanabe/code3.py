abb6937347297e7cc0222f2e2e623e5fd1d6a334
kondohyasuhiro <kondo.y.au@m.titech.ac.jp>
2017-10-18 12:23:40 +0900

    code3 has passed

--- watanabe/code3.py
+++ watanabe/code3.py
@@ -1,7 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import pymc as pm
+import pymc3 as pm
 from pymc3.backends.base import merge_traces
+import theano.tensor as T
 
 disasters_data = np.array([4, 5, 4, 0, 1, 4, 3, 4, 0, 6, 3, 3, 4, 0, 2, 6, 3, 3, 5, 4, 5, 3, 1, 4, 4, 1, 5, 5, 3, 4, 2, 5,
 2, 2, 3, 4, 2, 1, 3, 2, 2, 1, 1, 1, 1, 3, 0, 0,
@@ -13,15 +14,14 @@ years = len(disasters_data)
 plt.plot(disasters_data,".-")
 plt.savefig("img1.png")
 
-
 with pm.Model() as model_disaster:
     switchpoint = pm.DiscreteUniform('switchpoint', lower=0, upper=years)
     early_mean = pm.Exponential('early_mean', lam=1.)
     late_mean = pm.Exponential('late_mean', lam=1.)
     idx = np.arange(years)
-    rate = pm.switch(switchpoint >= idx, early_mean, late_mean)
+    rate = T.switch(switchpoint >= idx, early_mean, late_mean)
     disasters = pm.Poisson('disasters', rate, observed=disasters_data)
-    
+
 n=1000
 with model_disaster:
 	start = {'early_mean': 2., 'late_mean': 3.}
@@ -30,13 +30,15 @@ with model_disaster:
 	trace_disaster = pm.sample(n, tune=500, start=start, step=[step1, step2],progressbar=False)
 
 with model_disaster:
-	pm.trace.plot(trace_disaster,model_disaster.vars)
+	pm.traceplot(trace_disaster,model_disaster.vars)
 
 #結果の重ね書き
-
-for i in xrange(len(trace_disaster[early_mean])):
-    e=trace_disaster[early_mean][i]
-    l=trace_disaster[late_mean][i]
-    s=trace_disaster[switchpoint][i]
-    v=[ e if(y<s) else l for y in xrange(years)]
-    plt.plot(range(years),v,alpha=0.03,c='blue')
+#xrange is for python 2.6
+plt.show()
+with model_disaster:
+    for i in range(len(trace_disaster[early_mean])):
+        e=trace_disaster[early_mean][i]
+        l=trace_disaster[late_mean][i]
+        s=trace_disaster[switchpoint][i]
+        v=[ e if(y<s) else l for y in range(years)]
+        plt.plot(range(years),v,alpha=0.03,c='blue')
--- watanabe/code4.py
+++ watanabe/code4.py
@@ -13,21 +13,24 @@ nc.plot(figsize=(30,15),title="Close Price of NIKKEI index")
 plt.show()
 #plt.savefig("output/code4-fig2.png")
 returns.plot(title='return of NIKKEI index close price',figsize=(30,8))
-
+plt.show()
 nreturns=np.array(returns[1:])[::-1]
 
 import pymc3 as pm
 from pymc3.distributions.timeseries import GaussianRandomWalk
 from scipy.sparse import csc_matrix
 from scipy import optimize
+import theano.tensor as T
 
 with pm.Model() as model:
-    sigma, log_sigma = model.TransformedVar('sigma', pm.Exponential.dist(1./.02, testval=.1),
-                                            pm.logtransform)
+    #sigma, log_sigma = model.TransformedVar('sigma', pm.Exponential.dist(1./.02, testval=.1),
+    #pm.logtransform)
+    # TransformedVar was removed on 2015-6-13
+    sigma = pm.Exponential('sigma', 1. / .02, testval=1)
 
     nu = pm.Exponential('nu', 1./10)
     s = GaussianRandomWalk('s', sigma**-2, shape=len(nreturns))
-    r = pm.T('r', nu, lam=pm.exp(-2*s), observed=nreturns)
+    r = pm.Exponential('r', nu, lam=np.exp(-2*s), observed=nreturns)
 
 with model:
     start = pm.find_MAP(vars=[s], fmin=optimize.fmin_l_bfgs_b)
