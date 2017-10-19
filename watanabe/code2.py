17e031ceb3963444b0339a41e35de1690fc60155
kondohyasuhiro <kondo.y.au@m.titech.ac.jp>
2017-10-19 14:08:10 +0900

    paraneter

--- watanabe/code2.py
+++ watanabe/code2.py
@@ -5,29 +5,34 @@ import pandas as pd
 data=pd.read_csv("http://hosho.ees.hokudai.ac.jp/~kubo/stat/iwanamibook/fig/hbm/data7a.csv")
 
 #種子の数yごとに集計、グラフとして表示すると
+## from sum to count
 plt.bar(range(9),data.groupby('y').count().id)
 plt.savefig("output2/first.png")
-data.groupby('y').sum().T
+#data.groupby('y').count().T
 
 #dataの制限
-Y=np.array(data.y)[:6]
+Y=np.array(data.y)[:30]
 
 import numpy as np
 import pymc3 as pm
 import theano.tensor as T
+
 def invlogit(v):
 	return T.exp(v)/(T.exp(v)+1)
 
 with pm.Model() as model_hier:
-	s=pm.Uniform('s',0,1.0E+2)
-	beta=pm.Normal('beta',0,1.0E+2)
+	s=pm.Uniform('s',lower=0,upper=1.0E+4)
+	beta=pm.Normal('beta',0,1.0E-4)
 	r=pm.Normal('r',0,s,shape=len(Y))
 	q=invlogit(beta+r)
 	y=pm.Binomial('y',8,q,observed=Y)
-
-	step = pm.Slice([s,beta,r])
+	step = pm.Metropolis([s,beta,r])
+	#step = pm.Slice([s,beta,r])
 	trace_hier = pm.sample(10000, step)
 
 with model_hier:
 	pm.traceplot(trace_hier, model_hier.vars)
-	plt.savefig("output2/simple_linear_trace.png")
+	#plt.savefig("output2/simple_linear_trace.png")
+
+#with model_hier:
+#	plt.bar(range(9), trace_hier[y]);
